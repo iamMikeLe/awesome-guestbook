ebbe879583bbba6cb316fcf097a33b3e5931a344
Mike-ubuntu <iam@mike-le.info>
2024-03-20 09:55:25 +0100

    Create static "ADD NEW VISITOR" form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import StaticAppBar from "@/components/AppBar";
+import Homepage from "@/pages/Homepage";
 
 export default function App() {
   return (
     <>
       <StaticAppBar />
+      <Homepage />
     </>
   );
 }
