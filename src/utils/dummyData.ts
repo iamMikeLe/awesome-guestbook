2579054c098623e7148a28af6dbe65121dba4f50
Mike-ubuntu <iam@mike-le.info>
2024-03-20 11:41:34 +0100

    Update VisitorManagement component and add dummyData module

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,32 @@
+export const dummyData = [
+  {
+    selected: false,
+    visitor: "John",
+    email: "John@mail.com",
+    department: "marketing",
+  },
+  {
+    selected: true,
+    visitor: "Jane",
+    email: "Jane@mail.com",
+    department: "accounting",
+  },
+  {
+    selected: false,
+    visitor: "Emma",
+    email: "Emma@mail.com",
+    department: "management",
+  },
+  {
+    selected: true,
+    visitor: "Lucas",
+    email: "Lucas@mail.com",
+    department: "sales",
+  },
+  {
+    selected: false,
+    visitor: "Olivia",
+    email: "Olivia@mail.com",
+    department: "IT",
+  },
+];
